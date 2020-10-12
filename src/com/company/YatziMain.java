39cd44b2a1eff8290340993f362dc2a9843f7352
Andreas Berg <andreas.berg2@student.kyh.se>
2020-10-12 15:02:37 +0200

    Revert "lite förbättring av kod"
    This reverts commit 8ef0798b156106122f29ff02bb26f93486af8e3d.

--- src/com/company/YatziMain.java
+++ src/com/company/YatziMain.java
@@ -46,6 +46,7 @@ public class YatziMain {
                 if (sc.next().equals("y")) {
                     ++turn;
 
+
                 }else break; // avslutar programmet om man ej vill spela mer
             } else {
                 System.out.println("Game over! Want to play again?");
